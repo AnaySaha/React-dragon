d63dfb963120bf40855a2ff0caaf6129a37df83d
Anay Saha <anaysahapaban@gmail.com>
2025-02-23 03:16:57 +0600

    login and sign up page done

--- src/layouts/AuthLayout.jsx
+++ src/layouts/AuthLayout.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
-
+import Navbar from "../components/Navbar";
 
 const AuthLayout = () => {
     return (
-        <div>
-           AuthLayout
+        <div className="font-poppins bg-color accent-lime-700">
+           <header className="py-3 w-11/12 mx-auto">
+            <Navbar></Navbar>
+           </header>
            <Outlet></Outlet> 
         </div>
     );
---
+++ src/pages/Register.jsx
@@ -0,0 +1,45 @@
+import { Link } from "react-router-dom";
+
+
+const Register = () => {
+    return (
+        <div className="min-h-screen flex justify-center items-center hero bg-base-200">
+        <div className="card bg-base-100 w-full max-w-lg shrink-0 shadow-2xl rounded-none p-10">
+ <h2 className="text-2xl font-semibold text-center">Create Your Account</h2>
+ <form className="card-body">
+
+ <div className="form-control">
+     <label className="label">
+       <span className="label-text">Name</span>
+     </label>
+     <input type="text" placeholder="name" className="input input-bordered" required />
+   </div>
+
+   <div className="form-control">
+     <label className="label">
+       <span className="label-text">Email</span>
+     </label>
+     <input type="email" placeholder="email" className="input input-bordered" required />
+   </div>
+   <div className="form-control">
+     <label className="label">
+       <span className="label-text">Password</span>
+     </label>
+     <input type="password" placeholder="password" className="input input-bordered" required />
+     <label className="label">
+       <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+     </label>
+   </div>
+   <div className="form-control mt-6">
+     <button className="btn btn-neutral rounded-none">Sign Up</button>
+   </div>
+ </form>
+ <p className="text-center font-semibold">
+   Already Have an Account? Please <Link className="text-blue-500" to="/auth/login">Login</Link>
+ </p>
+</div>
+   </div>
+    );
+};
+
+export default Register;
