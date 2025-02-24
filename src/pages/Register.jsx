1ae9710edef78b615bfc9d77ad19b448b8e7c461
Anay Saha <anaysahapaban@gmail.com>
2025-02-25 03:10:53 +0600

    out

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,12 +1,39 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../Provider/AuthProvider";
 
 
 const Register = () => {
+
+const { createNewUser, user, setUser } = useContext(AuthContext);
+
+  const handleSubmit = (e)=> {
+    e.preventDefault();
+    //get from data
+    const form = new FormData(e.target);
+    const name = form.get("name");
+    const email = form.get("email");
+    const password = form.get("password");
+    console.log({ name, email, password });
+
+    createNewUser(email, password)
+    .then((result) => {
+      const user = result.user;
+      setUser(user);
+      console.log(user);
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log(errorCode, errorMessage);
+    });
+   
+  };
     return (
         <div className="min-h-screen flex justify-center items-center hero bg-base-200">
         <div className="card bg-base-100 w-full max-w-lg shrink-0 shadow-2xl rounded-none p-10">
  <h2 className="text-2xl font-semibold text-center">Create Your Account</h2>
- <form className="card-body">
+ <form onSubmit={handleSubmit} className="card-body">
 
  <div className="form-control">
      <label className="label">
@@ -25,7 +52,7 @@ const Register = () => {
      <label className="label">
        <span className="label-text">Password</span>
      </label>
-     <input type="password" placeholder="password" className="input input-bordered" required />
+     <input name="password" type="password" placeholder="password" className="input input-bordered" required />
      <label className="label">
        <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
      </label>
