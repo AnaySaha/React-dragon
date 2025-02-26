7477e13addc85dabe961bda5883d771513484ae9
Anay Saha <anaysahapaban@gmail.com>
2025-02-27 03:04:54 +0600

    last stage

--- src/Provider/AuthProvider.jsx
+++ src/Provider/AuthProvider.jsx
@@ -1,23 +1,30 @@
 import { createContext, useEffect, useState } from "react";
 import app from "../firebase/firebase.confi";
-import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth";
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut,  } from "firebase/auth";
 
 
 export const AuthContext = createContext();
 const auth = getAuth(app);
 const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null);
+
     console.log(user);
+    const [loading, setLoading] = useState(true);
+    console.log(loading, user);
 
     const createNewUser = (email, password) => {
+        setLoading(true);
         return createUserWithEmailAndPassword(auth, email, password)
     };
 
     const userLogin=(email,password)=> {
+        setLoading(true);
         return signInWithEmailAndPassword(auth, email, password);
+        
     };
 
     const logOut = () => {
+        setLoading(true);
         return signOut(auth);
     };
 
@@ -27,11 +34,13 @@ const AuthProvider = ({ children }) => {
         createNewUser,
         logOut,
         userLogin,
+        loading,
     };
 
     useEffect(()=>{
       const unsubscribe =  onAuthStateChanged(auth, currentUser=>{
             setUser(currentUser);
+            setLoading(false);
         });
         return () => {
             unsubscribe();
